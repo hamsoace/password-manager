cd30ff7369ee364124cf4f2bfd026c811b03b555
hamsoace <hamsoace@gmail.com>
2023-02-06 18:00:12 +0300

    first commit

---
+++ lib/flutter_calendar_week.dart
@@ -0,0 +1,2 @@
+export 'package:password_manager/src/calendar_week.dart';
+export 'package:password_manager/src/models/decoration_item.dart';
---
+++ lib/src/utils/find_current_week_index.dart
@@ -0,0 +1,22 @@
+import '../models/week_item.dart';
+import 'compare_date.dart';
+
+/// [findCurrentWeekIndexByDate] return -1 when cannot match
+int findCurrentWeekIndexByDate(DateTime dateTime, List<WeekItem> weeks) {
+  int index = -1;
+  bool matched = false;
+  for (int i = 0; i < weeks.length; i++) {
+    index++;
+    for (int j = 0; j < weeks[i].days.length; j++) {
+      if (compareDate(dateTime, weeks[i].days[j])) {
+        matched = true;
+        break;
+      }
+    }
+    if (matched) {
+      break;
+    }
+  }
+
+  return matched ? index : -1;
+}
